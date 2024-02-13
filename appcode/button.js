efaf1b515ab2cf2bac563d7b34ce1887a8ad7517
Chris Hodges <113991354+chodgesfigma@users.noreply.github.com>
2024-02-13 14:13:29 +0000

    Update button.js

--- appcode/button.js
+++ appcode/button.js
@@ -1,1 +1,22 @@
+export const Primary = {
+    args: {
+    buttonType: primary,
+    state: selected,
+    showIconLeft: false,
+    showLabel: true,
+    showIconRight: true,
+    label: button,
+  },
+};
+
+export const Primary = {
+    args: {
+    buttonType: secondary,
+    state: selected,
+    showIconLeft: false,
+    showLabel: true,
+    showIconRight: true,
+    label: button,
+  },
+};
 
